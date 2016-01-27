cad4b65a8aa2c488cc0a7a17d6166d1d51f8f520
David Tang <dtang859@gmail.com>
2016-01-27 01:43:00 -0800

    csp fix

--- config/environment.js
+++ config/environment.js
@@ -21,7 +21,7 @@ module.exports = function(environment) {
       // 'default-src': ["'none'"],
       // 'script-src':  ["'self'"],
       // 'font-src':    ["'self'"],
-      // 'connect-src': ["'self'", "https://api.github.com"],
+      'connect-src': "'self' https://api.github.com"
       // 'img-src':     ["'self'"],
       // 'style-src':   ["'self'"],
       // 'media-src':   ["'self'"]
